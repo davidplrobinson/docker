9869ce9fcd4e64dfd10f1a03dd8b9c0cb2b4c859
David Robinson <david.pl.robinson@googlemail.com>
2017-02-27 21:29:36 +1100

    Update set_remote.sh

--- set_remote.sh
+++ set_remote.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 git config credential.helper store
-git remote add docker_setup https://github.com/davidplrobinson/docker.git
+git remote add docker https://github.com/davidplrobinson/docker.git
