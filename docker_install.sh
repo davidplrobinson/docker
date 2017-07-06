20226462bea45344cd76c4147b853333e4a25531
David Robinson <david.pl.robinson@googlemail.com>
2017-07-07 08:43:38 +1000

    Update docker_install.sh

--- docker_install.sh
+++ docker_install.sh
@@ -10,5 +10,6 @@ apt-key fingerprint 0EBFCD88
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update
 apt-get install -y docker-ce
+apt-get install -y docker-compose
 
 docker run hello-world
