e0d4a91715ee7e2feb5f6e8e8935a78fe3610bb4
David Robinson <david.pl.robinson@googlemail.com>
2017-07-07 08:54:01 +1000

    Update docker_install.sh

--- docker_install.sh
+++ docker_install.sh
@@ -10,6 +10,8 @@ apt-key fingerprint 0EBFCD88
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update
 apt-get install -y docker-ce
-apt-get install -y docker-compose
+#apt-get install -y docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.14.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 
 docker run hello-world
