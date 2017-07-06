7702c169ebb6ce493dbd9107d7f56572ffc85948
David Robinson <david.pl.robinson@googlemail.com>
2017-07-06 22:20:30 +1000

    Update docker_install.sh

--- docker_install.sh
+++ docker_install.sh
@@ -7,7 +7,7 @@ apt-get install -y --no-install-recommends linux-image-extra-$(uname -r) linux-i
 apt-get install -y --no-install-recommends apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 apt-key fingerprint 0EBFCD88
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update
 apt-get install -y docker-ce
 
