9722c3ad92e2973aaddf4be8cce2a75d3f373d91
David Robinson <david.pl.robinson@googlemail.com>
2017-07-13 16:20:43 +1000

    updated docker_install.sh

--- docker_install.sh
+++ docker_install.sh
@@ -5,13 +5,10 @@
 apt-get update
 apt-get install -y --no-install-recommends linux-image-extra-$(uname -r) linux-image-extra-virtual
 apt-get install -y --no-install-recommends apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-apt-key fingerprint 0EBFCD88
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+curl -fsSL https://apt.dockerproject.org/gpg | sudo apt-key add -
+add-apt-repository "deb https://apt.dockerproject.org/repo/ ubuntu-$(lsb_release -cs) main"
 apt-get update
-apt-get install -y docker-ce
-#apt-get install -y docker-compose
-curl -L https://github.com/docker/compose/releases/download/1.14.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-
+apt-get -y install docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.10.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose
+chmod +x /usr/bin/docker-compose
 docker run hello-world
