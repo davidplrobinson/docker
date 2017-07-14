e71cd26c6d18a77899c54103321d7d56cdeeff39
David Robinson <david.pl.robinson@googlemail.com>
2017-07-14 20:42:49 +1000

    Update docker_install.sh

--- docker_install.sh
+++ docker_install.sh
@@ -11,4 +11,7 @@ apt-get update
 apt-get -y install docker-compose
 curl -L "https://github.com/docker/compose/releases/download/1.10.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose
 chmod +x /usr/bin/docker-compose
+update-rc.d dockerd defaults
+update-rc.d docker enable
+service docker start
 docker run hello-world
